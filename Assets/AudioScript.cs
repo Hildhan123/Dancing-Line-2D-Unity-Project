e5521deb9d58b9fc57a597d1518d8f54df087318
Hildhan123 <hildhan123@gmail.com>
2021-11-06 14:37:16 +0700

    Barulagi

---
+++ Assets/AudioScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    public AudioSource src;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(src.clip.length);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //src.PlayScheduled(1);
+        src.Play;
+    }
+}
--- Assets/Playercollision.cs
+++ Assets/Playercollision.cs
@@ -7,21 +7,24 @@ public class Playercollision : MonoBehaviour
     public Movement movement;
     public GameOver gameManager;
     public  GameObject Checkpoints;
+    public GameObject Checkpointpanel;
     public Vector2 spawnPoint;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.transform.tag == "Enemy")
         {
-            movement.enabled = false;
+            //movement.enabled = false;
             FindObjectOfType<GameOver>().Endgame();
         }
 
         if(collision.transform.tag == "Checkpoint")
         {
             spawnPoint = Checkpoints.transform.position;
+            Checkpointpanel.SetActive(true);
             Destroy(Checkpoints);
         }
     }
+   
 
 }
